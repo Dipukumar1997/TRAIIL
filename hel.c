37d577e1fecf79c7ce5b7fa03701921c73744807
Dipu Kumar <dk95074450@gmail.com>
2024-07-10 04:50:43 +0530

    fffff

--- hel.c
+++ hel.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    // printf("dhdy")
+    printf("dhdy")
     return 0;
     return 0;
     return 0;
-    
+      printf("dhdy")
+    return 0;
+    return 0;
+    return 0;
+    rintf("dhdy")
+    return 0;
+    return 0;
+    return 0;
+
+
 }
